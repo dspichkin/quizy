6848b8cb17ce892a0982cfb1624b88669457eff1
Denis Y. Spichkin <ds@levellab.ru>
2015-12-13 11:44:52 +0300

    fix

--- djangoapps/users/account/models.py
+++ djangoapps/users/account/models.py
@@ -221,6 +221,8 @@ class Account(AbstractUser):
         send_mail(subject, message, from_email, [self.email], **kwargs)
 
     def save(self, *args, **kwargs):
+        if not self.username and self.email:
+            self.email = self.username
         if self.email and self.email != self.username:
             self.username = self.email
         super(Account, self).save(*args, **kwargs)
